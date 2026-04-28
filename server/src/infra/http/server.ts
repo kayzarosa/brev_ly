a0714e6f4caf6e256fc7570d0089d48f723a7ffb
kayza <kayzakizy@gmail.com>
2026-04-27 21:18:29 -0300

    new: sub-footer

--- server/src/infra/http/server.ts
+++ server/src/infra/http/server.ts
@@ -1,6 +1,6 @@
 import { fastifyCors } from '@fastify/cors'
 import { fastifySwagger } from '@fastify/swagger'
-import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import scalarUI from '@scalar/fastify-api-reference'
 import { fastify } from 'fastify'
 import {
   hasZodFastifySchemaValidationErrors,
@@ -52,8 +52,12 @@ server.register(fastifySwagger, {
   transform: jsonSchemaTransform,
 })
 
-server.register(fastifySwaggerUi, {
+server.register(scalarUI, {
   routePrefix: '/docs',
+  configuration: {
+    layout: 'modern',
+    theme: 'bluePlanet'
+  }
 })
 
 server.register(healthCheckRoute)
